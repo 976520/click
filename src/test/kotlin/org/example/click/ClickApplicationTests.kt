f169d6421beaf34cf08027e16b123d79da5f606d
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-18 09:34:28 +0900

    test

--- src/test/kotlin/org/example/click/ClickApplicationTests.kt
+++ src/test/kotlin/org/example/click/ClickApplicationTests.kt
@@ -1,9 +1,19 @@
 package org.example.click
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
-@SpringBootTest
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension::class, MockitoExtension::class)
 class ClickApplicationTests {
 
 	@Test
